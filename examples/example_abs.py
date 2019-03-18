ba2d152a44c26a76a1d79e985123d5168e22fd8a
Jim Aman <jim.a.aman@gmail.com>
2019-03-18 09:23:35 -0500

    gotta get back to writing

--- examples/example_abs.py
+++ examples/example_abs.py
@@ -5,50 +5,49 @@ from scipy.special import hermite
 
 ######################################################
 #
-# Harmonic oscillator example
+# Absolute value example
 #
-# Stephen.Gibson@anu.edu.au
-#  24 Aug 2017
+# jim.aman@rice.edu
+#  16 March 2019
 ######################################################
 
 
 def phi(v, R, alpha):
     y = R*np.sqrt(alpha)
-    Nv = ((alpha/np.pi)**0.25)/np.sqrt(2**v*np.math.factorial(v))
+    Nv = (alpha/np.pi)**0.25/np.sqrt(2**v*np.math.factorial(v))
     Hv = hermite(v)
     sum = 0.0
     for i, h in enumerate(Hv.coeffs[::-1]):
         sum += h*(y**i)
     return (-1)**v*sum*Nv*np.exp(-y**2/2)
 
-μ = 2
-ω = 1/3
-R = np.linspace(-2, 2, 2001)
-# quadratic PEC
-V = (1/2)*μ*(ω*R)**2
 
-X = cse.Cse(μ, VT=[(R, V)])
+R = np.linspace(-2, 2, 401)
+# linear PEC
+V = abs(R)/10
+
+X = cse.Cse(2, VT=[(R, V)])
 
 fig, ax = plt.subplots()
 
-print(" v   E(v) [cm-1] | [eV]")
-for en in [0.01, 0.1, 0.16]:  # guess energies in eV
+print(" v    E(v) in cm-1")
+for en in [0.01, 0.03, 0.04, 0.06, 0.09]:  # guess energies in eV
     X.solve(en)
     wf = X.wavefunction[:, 0, 0]
     if X.vib % 2:
         wf = -wf
-    print(f'{X.vib:2d}      {X.cm:8.2f} |{X.cm/8065.541:8.4f}')
-    ax.plot(R, wf*300 + X.cm, label=fr'$v={X.vib:d}$')
-    ax.plot(R, phi(X.vib, R, μ*ω)*300 + X.cm, 'k--')
+    print(f'{X.vib:2d}  {X.cm:8.2f}')
+    ax.plot(R, wf*400 + X.cm, label=fr'$v={X.vib:d}$')
+    ax.plot(R, phi(X.vib, R, 10)*400 + X.cm, 'k--')
 
 ax.plot(np.NaN, np.NaN, 'k--', label=r'analytical')
 ax.plot(R, V*8065.541)
-ax.axis(xmin=-2, xmax=2, ymin=-10, ymax=2000)
+ax.axis(xmin=-1.5, xmax=1.5, ymin=-800, ymax=1600)
 ax.legend()
-ax.set_title(r'Harmonic oscillator potential:'
+ax.set_title(r'Absolute value potential:'
              r' $\psi_v(x) = N_v H_v(x) e^{-x^2/2}$')
 ax.set_ylabel(r'potential energy (cm$^{-1}$) / wavefunction $\times 400$')
 ax.set_xlabel(r'$x$')
 
-plt.savefig('output/example_HO.png', dpi=75)
+plt.savefig('output/example_abs.png', dpi=75)
 plt.show()
